343b7598a8978a04900bb8594b8bb4d5fd381743
ErickMadrid <127989412+ErickMadrid@users.noreply.github.com>
2025-07-13 16:46:44 -0300

    Update App.jsx

--- simpleswap-frontend/src/App.jsx
+++ simpleswap-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { ethers } from "ethers";
-import abi from "./abis/SimpleSwap.json";
+import SimpleSwap from "./abis/SimpleSwap.json";
 
 // 🔁 Reemplazá estas direcciones por las reales
 const SIMPLESWAP_ADDRESS = "0x342Cac67789e7dCD349B7c3Ba64476d656A16372";
